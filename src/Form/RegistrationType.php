9520a3abf483c7a1d6cff632910fa8b16f8bd530
glerique <gael.lerique@gmail.com>
2020-07-30 16:15:19 +0200

    Create RegistrationType Class

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Form\ApplicationType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationType extends ApplicationType
+{
+
+
+    
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, $this->getConfiguration("Prénom","Prénom"))
+            ->add('lastName', TextType::class, $this->getConfiguration("Nom","Nom"))
+            ->add('nickname', TextType::class, $this->getConfiguration("Pseudo","Pseudo"))
+            ->add('email', EmailType::class, $this->getConfiguration("Email","Email"))
+            ->add('password', PasswordType::class, $this->getConfiguration("Mot de passe","Mot de passe"))
+            ->add('confirmPassword', PasswordType::class, $this->getConfiguration("Confirmation du mot de passe","Confirmation du mot de passe"))
+            ->add('avatar', FileType::class,  [
+                'label' => 'Avatar',
+                'mapped' => false 
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
