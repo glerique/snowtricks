5faea79f2cd3537726f62091d9354eeb855074c9
glerique <gael.lerique@gmail.com>
2020-08-12 16:29:14 +0200

    Create ImageType Class

---
+++ src/Form/ImageType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use App\Form\ApplicationType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+class ImageType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+                 
+        $builder
+        ->add('file',
+            FileType::class,
+
+                [
+                    'required' => false,
+                    'attr' => [
+                        'placeholder' => 'Image de trick'
+                    ]
+                        ]) 
+        ->add('caption', TextType::class, $this->getConfiguration("Légende", "Légende de l'image"))                ; 
+
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
