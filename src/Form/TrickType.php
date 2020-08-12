a9431541f3a65db245c80a759619f7f9043ab057
glerique <gael.lerique@gmail.com>
2020-08-12 16:44:50 +0200

    Add image form

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use App\Entity\Trick;
+use App\Form\ImageType;
 use App\Entity\Category;
 use App\Form\ApplicationType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,24 +13,37 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class TrickType extends ApplicationType
 {
-    
+
 
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, $this->getConfiguration('Nom', 'Nom'))            
+            ->add('name', TextType::class, $this->getConfiguration('Nom', 'Nom'))
             ->add('description', TextareaType::class, $this->getConfiguration('Description', 'Description'))
             ->add('coverImage', FileType::class,  [
                 'label' => 'Image de couverture',
-                'mapped' => false, 
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Image de couverture'
+                ],
                 'required' => false
             ])
             ->add('category', EntityType::class, $this->getEntityConfiguration(Category::class, 'Categorie', 'name'))
-            ->add('user', EntityType::class, $this->getEntityConfiguration(User::class, 'Utilisateur', 'firstname'));
+            ->add('user', EntityType::class, $this->getEntityConfiguration(User::class, 'Utilisateur', 'firstname'))
+            ->add(
+                'images',
+                CollectionType::class,
+                [
+                    'entry_type' => ImageType::class,
+                    'allow_add' => true,
+                    'allow_delete' => true
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
